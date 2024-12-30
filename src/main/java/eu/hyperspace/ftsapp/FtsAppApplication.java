e22610c166f5f46eda5ff7c5f3db06d6492906ea
NKulbaka <nikita.kulbaka@mail.ru>
2024-12-30 13:42:37 +0300

    Initial commit

---
+++ src/main/java/eu/hyperspace/ftsapp/FtsAppApplication.java
@@ -0,0 +1,13 @@
+package eu.hyperspace.ftsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtsAppApplication.class, args);
+	}
+
+}
